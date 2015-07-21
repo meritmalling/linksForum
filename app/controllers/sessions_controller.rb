893e91d5526311706484c2935570736ab99afe32
merittrompeter <mktrompeter@gmail.com>
2015-07-20 22:42:25 -0700

    authentication working

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:user][:email],
+    params[:user][:password])
+    if user
+      #render json: params
+      flash[:success] = "Logged In"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:danger] = "Incorrect email or password."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:info] = "Logged Out"
+    redirect_to login_path
+  end
+
+end
